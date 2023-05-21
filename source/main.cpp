b7d652671dfe8659d1dd6393d2dc6c1ffa2f390b
Denis Cooper <denis.cooper09@gmail.com>
2023-05-21 13:45:16 +0100

    Link SFML: create window

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Meow" << std::endl;
-    system("pause>nul");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    const uint32_t WINDOW_WIDTH = 1200;
+    const uint32_t WINDOW_HEIGHT = 800;
+
+    sf::RenderWindow window({WINDOW_WIDTH, WINDOW_HEIGHT},
+                            "Game Of Life",
+                            sf::Style::Close,
+                            settings);
+
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::EventType::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
     return 0;
 }
