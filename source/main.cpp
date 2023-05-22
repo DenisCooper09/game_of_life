cf2f8b3a5f9006244085cfd741fbc51992d2ed6e
Denis Cooper <denis.cooper09@gmail.com>
2023-05-22 23:12:08 +0100

    Add GUI: generations count, generations per second, position of selected cell, and status of selected cell texts

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <sstream>
 #include "game_of_life_algorithm.h"
 #include "resources_loader.h"
+
+template<typename T>
+std::string to_string_with_precision(const T &value, const int &n = 1) {
+    std::ostringstream out;
+    out.precision(n);
+    out << std::fixed << value;
+    return std::move(out).str();
+}
 
 int main() {
     sf::Font main_font;
@@ -17,25 +26,84 @@ int main() {
     const uint32_t WINDOW_WIDTH = 1200;
     const uint32_t WINDOW_HEIGHT = 800;
 
+    float generations_per_second = 1.0f;
+
     sf::RenderWindow window({WINDOW_WIDTH, WINDOW_HEIGHT},
                             "Game Of Life",
                             sf::Style::Close,
                             settings);
 
-    game_of_life_algorithm game_of_life(WINDOW_WIDTH, WINDOW_HEIGHT, 20);
+    sf::Text generations_count_text;
+    generations_count_text.setFont(main_font);
+    generations_count_text.setFillColor(sf::Color::Green);
+    generations_count_text.setPosition(WINDOW_HEIGHT + 15, 10);
+    generations_count_text.setCharacterSize(24);
+
+    sf::Text generations_per_second_text;
+    generations_per_second_text.setFont(main_font);
+    generations_per_second_text.setFillColor(sf::Color::Green);
+    generations_per_second_text.setPosition(WINDOW_HEIGHT + 15, 50);
+    generations_per_second_text.setCharacterSize(24);
+
+    sf::Text selected_cell_position_text;
+    selected_cell_position_text.setFont(main_font);
+    selected_cell_position_text.setFillColor(sf::Color::Green);
+    selected_cell_position_text.setPosition(WINDOW_HEIGHT + 15, 90);
+    selected_cell_position_text.setCharacterSize(24);
+
+    sf::Text selected_cell_state_text;
+    selected_cell_state_text.setFont(main_font);
+    selected_cell_state_text.setFillColor(sf::Color::Green);
+    selected_cell_state_text.setPosition(WINDOW_HEIGHT + 15, 130);
+    selected_cell_state_text.setCharacterSize(24);
+
+    game_of_life_algorithm game_of_life(WINDOW_HEIGHT, WINDOW_HEIGHT, 20);
 
     while (window.isOpen()) {
         sf::Event event{};
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::EventType::Closed) {
                 window.close();
+            } else if (event.type == sf::Event::EventType::MouseWheelScrolled) {
+                generations_per_second += event.mouseWheelScroll.delta * 0.5f;
+                if (generations_per_second > 20.0f) {
+                    generations_per_second = 1.0f;
+                } else if (generations_per_second <= 0.0f) {
+                    generations_per_second = 20.0f;
+                }
             }
         }
 
         game_of_life.update(window, event);
+        game_of_life.set_delay(1.0f / generations_per_second);
+
+        generations_count_text.setString(
+                "Generation: " + std::to_string(game_of_life.get_generations_count())
+        );
+
+        generations_per_second_text.setString(
+                "Generations/second: " + to_string_with_precision(generations_per_second)
+        );
+
+        selected_cell_position_text.setString(
+                "X: " + std::to_string(game_of_life.get_selected_cell_position().x) +
+                " Y: " + std::to_string(game_of_life.get_selected_cell_position().y)
+        );
+
+        selected_cell_state_text.setString((game_of_life.get_cell(
+                game_of_life.get_selected_cell_position().x,
+                game_of_life.get_selected_cell_position().y) ? "ALIVE" : "DEAD")
+        );
 
         window.clear(sf::Color::Black);
+
         game_of_life.draw_cells(window);
+
+        window.draw(generations_count_text);
+        window.draw(generations_per_second_text);
+        window.draw(selected_cell_position_text);
+        window.draw(selected_cell_state_text);
+
         window.display();
     }
 
